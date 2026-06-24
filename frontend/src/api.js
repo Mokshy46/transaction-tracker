94b6e1e11f395f82ce569e43244c06341df717bc
Mokshy <mokshithgowda88@gmail.com>
2026-06-24 19:22:33 +0530

    fixed API base URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://transaction-tracker-bpiw.onrender.com"
+  baseURL: "https://transaction-tracker-bpiw.onrender.com/api"
 });
